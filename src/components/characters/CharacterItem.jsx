7641a8887c0362876e62c41eba7365223e8f41d7
Abdulaha Islam <abdulahaislam210917@gmail.com>
2021-12-09 16:23:52 +0600

    Commit Characters Name

---
+++ src/components/characters/CharacterItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CharacterItem = ({item}) => {
+    return (
+        <div>
+            {item.name}
+        </div>
+    )
+}
+
+// CharacterItem.propTypes = {
+
+// }
+
+export default CharacterItem
